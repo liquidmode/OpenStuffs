48b082443375feb40c400b8bb84bc3c6ffbaef33
Liquid <101341072+liquidmode@users.noreply.github.com>
2024-10-04 15:27:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Optional: Smooth scrolling effect for buttons or other interactive elements
+document.querySelector('.explore-btn').addEventListener('click', () => {
+    window.scrollTo({
+        top: document.querySelector('.gallery-section').offsetTop,
+        behavior: 'smooth'
+    });
+});
+document.getElementById('discover-more-btn').addEventListener('click', function() {
+    var moreContent = document.getElementById('more-content');
+    if (moreContent.classList.contains('show')) {
+        moreContent.classList.remove('show');
+    } else {
+        moreContent.classList.add('show');
+    }
+});
+document.querySelectorAll('.more-item').forEach(item => {
+    item.addEventListener('click', function() {
+        // Get the URL from the data-page attribute
+        const pageUrl = this.getAttribute('data-page');
+        // Redirect to the corresponding page
+        window.location.href = pageUrl;
+    });
+});
+document.querySelectorAll('.service-item').forEach(item => {
+    item.addEventListener('click', function() {
+        const pageUrl = this.getAttribute('data-page');
+        window.location.href = pageUrl; // Redirect to the corresponding page
+    });
+});
